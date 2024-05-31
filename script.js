17bdaeed507bdc2c63185d7f0ea218c7129a1cef
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-31 16:55:54 -0300

    seletores-2

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ console.log(linkInterno);
 
 const animaisImg = document.querySelectorAll('.animais img');
 console.log(animaisImg);
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+primeiraUl.classList.add('grid-section');
+
+//console.log(gridSectionHTML);
+//console.log(gridSectionNode);
+
+gridSectionNode.forEach(function(item, index) {
+  console.log(item);
+});
+
+const arrayGrid = Array.from(gridSectionHTML);
+arrayGrid.forEach(function(item, index) {
+  console.log(item);
+});
