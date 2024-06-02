978d24432cbb72791f690f82f6eb24a1258e9859
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-06-02 16:15:29 -0300

    exercicios traversing

--- script.js
+++ script.js
@@ -52,3 +52,19 @@ const faq = document.querySelector(".faq");
 const cloneH1 = h1.cloneNode(true);
 
 faq.appendChild(cloneH1);
+
+// Duplique o menu e adicione ele em copy
+const menu = document.querySelector(".menu");
+const copy = document.querySelector(".copy");
+
+const cloneMenu = menu.cloneNode(true);
+
+copy.append(cloneMenu);
+// Selecione o primeiro DT da dl de Faq
+const primeiroDt = faq.querySelector("dt");
+
+// Selecione o DD referente ao primeiro DT
+const proxDd = primeiroDt.nextElementSibling;
+// Substitua o conteúdo html de .faq pelo de .animais
+const animais = document.querySelector(".animais");
+faq.innerHTML = animais.innerHTML;
